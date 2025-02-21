ca009de10a3e1855629acb217473a878158f6ecf
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2025-02-21 11:24:59 +0100

    Init project

---
+++ src/main/java/com/epicode/BE_U2_W3_D5_GestioneEventi/BeU2W3D5GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.BE_U2_W3_D5_GestioneEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W3D5GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W3D5GestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/BE_U2_W3_D5_GestioneEventi/BeU2W3D5GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.BE_U2_W3_D5_GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W3D5GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
